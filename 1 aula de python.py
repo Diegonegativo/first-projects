2e3f9c8d14468ad916078ba3d1dce056d96b724c
Diego gonçalves maria <141463519+Diegonegativo@users.noreply.github.com>
2023-10-30 21:44:31 -0300

    Add files via upload

---
+++ 1 aula de python.py
@@ -0,0 +1,32 @@
+print("ola mundo")
+
+print('digite 1 para somar 2 para subtrair 3 para multiplicar 4 para dividir ')
+escolha =(input('Digite qual operaçao voce quer:'))
+if escolha == '1':
+    a =int(input("Digite o 1 numero:"))
+if escolha == '2':
+    a =int(input("Digite o 1 numero:"))
+if escolha == '3':
+        a =int(input("Digite o 1 numero:"))
+if escolha == '4':
+         a =int(input("Digite o 1 numero:"))
+if escolha == '1':
+     b =int(input("Digite o 2 numero:"))
+if escolha == '2':
+     b =int(input("Digite o 2 numero:"))
+if escolha == '3':
+     b =int(input("Digite o 2 numero:"))
+if escolha == '4':
+     b =int(input("Digite o 2 numero:"))
+
+if escolha == '1':
+    print(a+b)
+elif escolha =='2' :
+    print(a-b)
+elif escolha =='3' :
+    print(a*b)
+elif escolha == '4' :
+    print(a/b)
+else:
+    print('invalido')
+
